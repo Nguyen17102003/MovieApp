e800ee08852ed202968acf00421d185dab1fd9eb
Bảo Nguyên <128112673+Nguyen17102003@users.noreply.github.com>
2025-09-10 22:34:22 +0700

    Fix

--- src/components/DetailComponents/DetailBanner.tsx
+++ src/components/DetailComponents/DetailBanner.tsx
@@ -11,12 +11,12 @@ const DetailBanner:React.FC<detailBannerProps> = ({movie, casts}) => {
       : "none"
     }}>
         {/* Poster phim bên trái */}
-        <div className='hidden w-full md:basis-1/3 z-30 md:px-2 xl:px-4 md:flex'>
+        <div className='hidden w-full md:basis-1/3 2xl:basis:2/5 z-30 md:px-2 xl:px-4 2xl:px-0 md:flex'>
             <img loading='lazy' src={`https://image.tmdb.org/t/p/w500/${movie?.poster_path}`} alt={movie?.title} className='w-full h-full rounded-xl md:rounded-2xl lg:rounded-3xl object-cover object-center aspect-auto' />
         </div>
         
         {/* Thông tin phim bên phải */}
-        <div className='flex flex-col justify-baseline md:basis-2/3 gap-7 md:gap-5 lg:gap-10 z-30'>
+        <div className='flex flex-col justify-baseline md:basis-3/5 gap-7 md:gap-5 lg:gap-10 z-30'>
             <h1 className='text-2xl md:text-4xl xl:text-8xl 2xl:text-[8vh] text-white font-bold'>{movie?.title || movie?.name}</h1>
             <div className='flex gap-2 2xl:gap-[0.5vw] py-5 md:py-0'>
                 {movie?.genres && movie.genres.map((genre: genre) => (
@@ -27,7 +27,7 @@ const DetailBanner:React.FC<detailBannerProps> = ({movie, casts}) => {
                 {movie?.overview}
             </div>
             <h2 className='text-white font-semibold lg:text-2xl 2xl:text-[5vh]'>Casts</h2>
-            <div className='md:flex lg:gap-5 2xl:gap-20 gap-3 lg:pb-15 grid grid-cols-3' >
+            <div className='md:flex lg:gap-5 2xl:gap-10 gap-3 lg:pb-15 grid grid-cols-3' >
                 {casts && casts.map((cast: cast, i: number) => (
                     <div key={i} className='md:w-full xl:w-25 2xl:w-[10vw]'>
                         <img src={`https://image.tmdb.org/t/p/original/${cast.profile_path}`} className='rounded-xl md:rounded-2xl lg:h-40 2xl:h-[30vh] 2xl:mb-[2vh] lg:w-full aspect-auto'/>
